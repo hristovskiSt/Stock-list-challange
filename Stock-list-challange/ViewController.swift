7b9f184befbf29184720c7c0ab7950f5088c1c1d
Stefan Hristovski <hristovski.stefan1@gmail.com>
2022-01-29 10:41:32 +0100

    Initial commit
    Initial commit

---
+++ Stock-list-challange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stock-list-challange
+//
+//  Created by Stefan Hristovski on 29.1.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Stock-list-challangeUITests/Stock_list_challangeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Stock_list_challangeUITestsLaunchTests.swift
+//  Stock-list-challangeUITests
+//
+//  Created by Stefan Hristovski on 29.1.22.
+//
+
+import XCTest
+
+class Stock_list_challangeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
